6f8f38d05ab30f67939322a62a9b069d19c463f9
fh2412 <florianhofer024@gmail.com>
2024-05-22 11:33:27 +0200

    creating memorise; modal

--- sites/memorise.js
+++ sites/memorise.js
@@ -3,36 +3,47 @@ document.addEventListener('DOMContentLoaded', () => {
     function openModal($el) {
       $el.classList.add('is-active');
     }
-  
+    
     function closeModal($el) {
       $el.classList.remove('is-active');
     }
-  
+    
     function closeAllModals() {
       (document.querySelectorAll('.modal') || []).forEach(($modal) => {
         closeModal($modal);
       });
     }
-  
-    // Add a click event on buttons to open a specific modal
+    
+    // Add a click event on buttons to open a specific modal with image update
     (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
       const modal = $trigger.dataset.target;
       const $target = document.getElementById(modal);
-  
-      $trigger.addEventListener('click', () => {
+      
+      $trigger.addEventListener('click', (event) => {
+        const clickedElement = event.currentTarget;
         openModal($target);
+        
+        const modalImage = document.getElementById("modal-image");
+        // Check if clicked element has src attribute (assuming it's an image)
+        if (clickedElement.src) {
+          modalImage.src = clickedElement.src;
+        } else {
+          // Otherwise, use data attribute for image path
+          const imageSrc = clickedElement.dataset.imageSrc;
+          modalImage.src = imageSrc;
+        }
       });
     });
-  
+    
     // Add a click event on various child elements to close the parent modal
     (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
       const $target = $close.closest('.modal');
-  
+      
       $close.addEventListener('click', () => {
         closeModal($target);
       });
     });
-  
+    
     // Add a keyboard event to close all modals
     document.addEventListener('keydown', (event) => {
       if(event.key === "Escape") {
@@ -40,3 +51,4 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+  
